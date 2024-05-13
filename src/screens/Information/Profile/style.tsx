ac8b1d20af7b92969010ac21b5e2eda30f5e3872
H-Thien11 <vlht2003@gmail.com>
2024-05-13 09:50:48 +0700

    hoan thanh

--- src/screens/Information/Profile/style.tsx
+++ src/screens/Information/Profile/style.tsx
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
   //image
   containerImg: {
     alignItems: 'center',
-    marginVertical: '5%',
+    marginTop: '5%',
   },
   //information
   containerInformation: {
